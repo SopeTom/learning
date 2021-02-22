ce346305c5beab49e9c2e25e5a5bf29f9c92710b
SopeTom <77493777+SopeTom@users.noreply.github.com>
2021-02-22 23:09:24 +0800

    找两个数的最大公因数

---
+++ 找两个数的最大公因数.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int u = 0;
+	int i = 0;
+	int temp = 0;
+	int n = 0;
+	int m = 0;
+	printf("请输入需要查找最大公约数的两个数:>");
+	scanf("%d%d", &u, &i);
+	n = u;
+	m = i;
+	if (i > u)
+	{
+		temp = i;
+		i = u;
+		u = temp;
+	}
+	while(u%i)
+	{
+		temp = u % i;
+		u = i;
+		i = temp;
+	}
+	printf("%d与%d的最小公因数为%d", n, m, i);
+	return 0;
+}
